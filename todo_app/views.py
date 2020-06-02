c37d39e07ea81f29800c01e12ccc36d714b16583
kunalkini15 <kunalkini15@gmail.com>
2020-06-02 17:02:33 +0530

    handled api exceptions

--- todo_app/views.py
+++ todo_app/views.py
@@ -31,7 +31,7 @@ class Register(APIView):
 
         try:
             user = User.objects.get(email=email)
-            return Response("User already exists", status=status.HTTP_409_CONFLICT)
+            return Response("User with this email already exists", status=status.HTTP_409_CONFLICT)
         except:
             user = User.objects.create_user(first_name=name, username=email, email=email, password=password)
             user.save()
@@ -43,6 +43,7 @@ class Login(APIView):
         email = request.data["email"]
         password = request.data["password"]
         try:
+            user_obj = User.objects.get(email=email)
             user = authenticate(username=email, password=password)
             if user is not None:
                 try:
@@ -51,192 +52,209 @@ class Login(APIView):
                         "message": "User Logged in successfully",
                         "subscribed": True
                     }
-                    print(data)
                     return Response(data, status=status.HTTP_200_OK)
                 except:
                     data={
                         "message": "User Logged in successfully",
                         "subscribed": False
                     }
-                    print(data)
                     return Response(data, status=status.HTTP_200_OK)
             else:
-                return Response("Wrong password", status=status.HTTP_401_UNAUTHORIZED)
+                return Response("You have entered the wrong password, Please try again!!", status=status.HTTP_401_UNAUTHORIZED)
         except:
-            return Response("User doesn't exist", status=status.HTTP_404_NOT_FOUND)
+            return Response("User with this email doesn't exist", status=status.HTTP_404_NOT_FOUND)
 
 class ListView(APIView):
     def get(self, request):
-        email = request.GET["email"]
-        user = User.objects.get(email=email)
-        list_objects = List.objects.filter(user=user)
-        response =[]
-        for list_object in list_objects:
-            response.append({
-                "id": list_object.id,
-                "name":list_object.name,
-            })
-        return JsonResponse(response, safe=False)
+        try:
+            email = request.GET["email"]
+            user = User.objects.get(email=email)
+            list_objects = List.objects.filter(user=user)
+            response =[]
+            for list_object in list_objects:
+                response.append({
+                    "id": list_object.id,
+                    "name":list_object.name,
+                })
+            return JsonResponse(response, safe=False)
+        except:
+            return Response("Error while fetching lists", status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
 
 
     def post(self, request):
-        name = request.data["name"]
-        description = request.data["description"]
-        email = request.data["email"]
+        try:
+            name = request.data["name"]
+            description = request.data["description"]
+            email = request.data["email"]
 
-        user = User.objects.get(email=email)
-
-
-        list_obj = List.objects.create(user=user, name=name, description=description)
-
-        return Response("List created successfully", status=status.HTTP_201_CREATED)
+            user = User.objects.get(email=email)
 
 
+            list_obj = List.objects.create(user=user, name=name, description=description)
+
+            return Response("List created successfully", status=status.HTTP_201_CREATED)
+        except:
+            return Response("Something went wrong", status=status.HTTP_409_CONFLICT)
 
 class TaskView(APIView):
 
     def get(self, request, task_id=0):
-        if task_id == 0:
-            list_id = request.GET["id"]
+        try:
+            if task_id == 0:
+                list_id = request.GET["id"]
 
-            for i in request.GET:
-                print(i, request.GET[i])
-            tasks = Task.objects.filter(list_obj=List.objects.get(pk=list_id))
+                tasks = Task.objects.filter(list_obj=List.objects.get(pk=list_id))
 
-            # applying filters
-            try:
-                progress = request.GET["progress"]
-                progress_filter = True
-            except:
-                progress = "active"
-                progress_filter=True
+                # applying filters
+                try:
+                    progress = request.GET["progress"]
+                    progress_filter = True
+                except:
+                    progress = "active"
+                    progress_filter=True
 
-            try:
-                label = request.GET["label"]
-                label_filter = True
-            except:
-                label_filter=False
+                try:
+                    label = request.GET["label"]
+                    label_filter = True
+                except:
+                    label_filter=False
 
-            try:
-                task_status = request.GET["status"]
-                status_filter = True
-            except:
-                status_filter=False
+                try:
+                    task_status = request.GET["status"]
+                    status_filter = True
+                except:
+                    status_filter=False
 
-            try:
-                priority = request.GET["priority"]
-                priority_filter = True
-            except:
-                priority_filter=False
+                try:
+                    priority = request.GET["priority"]
+                    priority_filter = True
+                except:
+                    priority_filter=False
 
-            if progress_filter: # applying progress filters
-                if progress == "Completed":
-                    filter=True
-                else:
-                    filter=False
-                tasks = tasks.filter(isCompleted=filter)
+                if progress_filter: # applying progress filters
+                    if progress == "Completed":
+                        filter=True
+                    else:
+                        filter=False
+                    tasks = tasks.filter(isCompleted=filter)
 
-            if label_filter:
-                label_obj = Label.objects.get(name=label)
-                tasks = tasks.filter(label=label_obj)
+                if label_filter:
+                    label_obj = Label.objects.get(name=label)
+                    tasks = tasks.filter(label=label_obj)
 
-            if status_filter:
-                status_obj = Status.objects.get(status=task_status)
-                tasks = tasks.filter(status=status_obj)
+                if status_filter:
+                    status_obj = Status.objects.get(status=task_status)
+                    tasks = tasks.filter(status=status_obj)
 
-            # add priority filter here
+                # add priority filter here
 
 
-            response = []
-            for task in tasks:
-                print(task.due_date.date() == datetime.date.today())
+                response = []
+                for task in tasks:
+                    current_task_object = {
+                    "id": task.id,
+                    "name": task.name,
+                    "description": task.description,
+                    "start_date": task.start_date,
+                    "due_date": task.due_date,
+                    "label": task.label.name,
+                    "status": task.status.status,
+                    "isCompleted": task.isCompleted
+                    }
+                    response.append(current_task_object)
+                return JsonResponse(response, safe=False)
+            else:
+                task = Task.objects.get(pk=task_id)
                 current_task_object = {
-                "id": task.id,
-                "name": task.name,
-                "description": task.description,
-                "start_date": task.start_date,
-                "due_date": task.due_date,
-                "label": task.label.name,
-                "status": task.status.status,
-                "isCompleted": task.isCompleted
+                    "id": task.id,
+                    "name": task.name,
+                    "description": task.description,
+                    "start_date": task.start_date,
+                    "due_date": task.due_date,
+                    "label": task.label.name,
+                    "status": task.status.status,
+                    "isCompleted": task.isCompleted
                 }
-                response.append(current_task_object)
-            return JsonResponse(response, safe=False)
-        else:
-            task = Task.objects.get(pk=task_id)
-            current_task_object = {
-                "id": task.id,
-                "name": task.name,
-                "description": task.description,
-                "start_date": task.start_date,
-                "due_date": task.due_date,
-                "label": task.label.name,
-                "status": task.status.status,
-                "isCompleted": task.isCompleted
-            }
-            return JsonResponse(current_task_object)
+                return JsonResponse(current_task_object)
+
+        except:
+            return Response("Error while fetching tasks", status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+
 
     def post(self, request):
-        list_id = request.data["id"]
-        name = request.data["name"]
-        description = request.data["description"]
-        label = request.data["label"]
-        task_status = request.data["status"]
-        task_date = request.data["date"]
-        task_time = request.data["time"]
-        split_date = list(map(int, task_date.split("-")))
-        split_time = list(map(int, task_time.split(":")))
-        due_date = (datetime.datetime(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]))
-        list_obj = List.objects.get(id=list_id)
-
-
-        label_obj = Label.objects.get(name=label)
-
-        status_obj = Status.objects.get(status=task_status)
-
-        task_obj = Task.objects.create(name=name, description=description, due_date=due_date,
-                                        list_obj=list_obj, label=label_obj, status=status_obj
-                                        )
-
-
-        return Response("Task created successfully", status=status.HTTP_201_CREATED)
-
-    def put(self, request):
-        task_id = request.data["id"]
-        task_obj = Task.objects.get(pk=task_id)
-        if "name" in request.data:
-            task_obj.name = request.data["name"]
-        if "description" in request.data:
-            task_obj.description = request.data["description"]
-
-        if "label" in request.data:
-            task_obj.label = Label.objects.get(name=request.data["label"])
-
-        if "status" in request.data:
-            task_obj.status = Status.objects.get(status=request.data["status"])
-
-        if "date" in request.data and "time" in request.data:
+        try:
+            list_id = request.data["id"]
+            name = request.data["name"]
+            description = request.data["description"]
+            label = request.data["label"]
+            task_status = request.data["status"]
             task_date = request.data["date"]
             task_time = request.data["time"]
             split_date = list(map(int, task_date.split("-")))
             split_time = list(map(int, task_time.split(":")))
             due_date = (datetime.datetime(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]))
-            task_obj.due_date = due_date
-        if "isCompleted" in request.data:
-            task_obj.isCompleted = request.data["isCompleted"]
-        task_obj.save()
+            list_obj = List.objects.get(id=list_id)
 
-        return JsonResponse("Task Updated successfully", safe=False)
+
+            label_obj = Label.objects.get(name=label)
+
+            status_obj = Status.objects.get(status=task_status)
+
+            task_obj = Task.objects.create(name=name, description=description, due_date=due_date,
+                                            list_obj=list_obj, label=label_obj, status=status_obj
+                                            )
+
+
+            return Response("Task created successfully", status=status.HTTP_201_CREATED)
+        except:
+            return Response("Something went wrong", status=status.HTTP_409_CONFLICT)
+
+
+    def put(self, request):
+        try:
+
+            task_id = request.data["id"]
+            task_obj = Task.objects.get(pk=task_id)
+            if "name" in request.data:
+                task_obj.name = request.data["name"]
+            if "description" in request.data:
+                task_obj.description = request.data["description"]
+
+            if "label" in request.data:
+                task_obj.label = Label.objects.get(name=request.data["label"])
+
+            if "status" in request.data:
+                task_obj.status = Status.objects.get(status=request.data["status"])
+
+            if "date" in request.data and "time" in request.data:
+                task_date = request.data["date"]
+                task_time = request.data["time"]
+                split_date = list(map(int, task_date.split("-")))
+                split_time = list(map(int, task_time.split(":")))
+                due_date = (datetime.datetime(split_date[0], split_date[1], split_date[2], split_time[0], split_time[1]))
+                task_obj.due_date = due_date
+            if "isCompleted" in request.data:
+                task_obj.isCompleted = request.data["isCompleted"]
+            task_obj.save()
+
+            return JsonResponse("Task Updated successfully", safe=False)
+        except:
+            return Response("Can't update the task at this moment", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
     def delete(self, request):
-        task_id = int(request.data["id"])
-        print(task_id)
-        t = Task.objects.get(pk=task_id)
-        t.delete()
-
-        return JsonResponse("Deleted successfully", safe=False)
+        try:
+            task_id = int(request.data["id"])
+            try:
+                t = Task.objects.get(pk=task_id)
+                t.delete()
+                return JsonResponse("Deleted successfully", safe=False)
+            except:
+                return Response("Task Not found", status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response("Something went wrong!", status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 
@@ -255,7 +273,6 @@ class SubscriptionView(APIView):
     def post(self, request):
         email = request.data["email"]
         subscribe = request.data["subscribe"]
-        print(email, subscribe)
         if subscribe:
             try:
                 user = Subscription.objects.get(user=User.objects.get(email=email))
